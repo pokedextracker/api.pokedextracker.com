794c7ad873864cb80b2c34d6af8b568b6e02e187
Robin Joseph <robinmjoseph08@gmail.com>
2023-06-18 11:35:21 -0500

    delete everything node related

--- src/models/dex-type-pokemon.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const Bookshelf = require('../libraries/bookshelf');
-
-module.exports = Bookshelf.model('DexTypePokemon', Bookshelf.Model.extend({
-  tableName: 'dex_types_pokemon'
-}));
