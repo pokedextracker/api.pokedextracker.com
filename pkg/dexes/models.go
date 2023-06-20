a2935f48484620fc32992b9e46ac4e624cbb3fe5
Robin Joseph <robinmjoseph08@gmail.com>
2023-06-20 10:20:37 -0500

    customize model serialization for parity tests

--- pkg/dexes/models.go
+++ pkg/dexes/models.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/pokedextracker/api.pokedextracker.com/pkg/dextypes"
 	"github.com/pokedextracker/api.pokedextracker.com/pkg/games"
+	"github.com/segmentio/encoding/json"
 )
 
 type Dex struct {
@@ -24,4 +25,18 @@ type Dex struct {
 	Total        int               `pg:"-" json:"total"`
 	DateCreated  time.Time         `json:"date_created"`
 	DateModified time.Time         `json:"date_modified"`
+}
+
+// MarshalJSON is just needed for parity testing. Once we're actually using this in production, we can remove it.
+func (d *Dex) MarshalJSON() ([]byte, error) {
+	type Alias Dex
+	return json.Marshal(&struct {
+		*Alias
+		DateCreated  string `json:"date_created"`
+		DateModified string `json:"date_modified"`
+	}{
+		Alias:        (*Alias)(d),
+		DateCreated:  d.DateCreated.Format("2006-01-02T15:04:05.000Z07:00"),
+		DateModified: d.DateModified.Format("2006-01-02T15:04:05.000Z07:00"),
+	})
 }
--- pkg/users/models.go
+++ pkg/users/models.go
@@ -4,6 +4,7 @@ import (
 	"time"
 
 	"github.com/pokedextracker/api.pokedextracker.com/pkg/dexes"
+	"github.com/segmentio/encoding/json"
 )
 
 type User struct {
@@ -22,4 +23,18 @@ type User struct {
 	Donated          bool         `pg:"-" json:"donated"`
 	DateCreated      time.Time    `json:"date_created"`
 	DateModified     time.Time    `json:"date_modified"`
+}
+
+// MarshalJSON is just needed for parity testing. Once we're actually using this in production, we can remove it.
+func (u *User) MarshalJSON() ([]byte, error) {
+	type Alias User
+	return json.Marshal(&struct {
+		*Alias
+		DateCreated  string `json:"date_created"`
+		DateModified string `json:"date_modified"`
+	}{
+		Alias:        (*Alias)(u),
+		DateCreated:  u.DateCreated.Format("2006-01-02T15:04:05.000Z07:00"),
+		DateModified: u.DateModified.Format("2006-01-02T15:04:05.000Z07:00"),
+	})
 }
