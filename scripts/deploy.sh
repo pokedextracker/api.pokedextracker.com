15208f0b61bab244fc71123600d68ddc3b2b477f
Robin Joseph <robinmjoseph08@gmail.com>
2023-07-01 19:40:11 -0500

    chore(deploy): move to fly.io

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,20 +1,9 @@
 #!/bin/bash -e
 
-CHART_VERSION=v0.1.0
 NOW=$(date +'%s')
 REPO="pokedextracker/api.pokedextracker.com"
 TAG="$(git rev-parse --short HEAD)"
 [[ -z $(git status -s) ]] || TAG="${TAG}-dirty-${NOW}"
-KUBE_CONTEXTS="$(kubectl config get-contexts -o name)"
-
-if ! echo "${KUBE_CONTEXTS}" | grep -q pokedextracker; then
-  echo
-  echo -e "\033[1;31m==> Couldn't find kube context with 'pokedextracker' in it. Make sure the cluster is configured correctly.\033[0m"
-  echo
-  exit 1
-fi
-
-KUBE_CONTEXT="$(echo "${KUBE_CONTEXTS}" | grep pokedextracker)"
 
 if ! docker buildx ls | grep -q multiarch; then
   echo
@@ -41,55 +30,13 @@ DOCKER_BUILDKIT=1 docker buildx build \
   .
 
 echo
-echo -e "\033[1;32m==> Updating Helm repos\033[0m"
+echo -e "\033[1;32m==> Deploying ${TAG} to staging...\033[0m"
 echo
 
-helm repo update
-
-echo
-echo -e "\033[1;32m==> Running migrations from ${TAG} on staging...\033[0m"
-echo
-
-kubectl run \
-  --context ${KUBE_CONTEXT} \
-  migrations-${TAG} \
-  --namespace staging \
-  --image=${REPO}:${TAG} \
-  --restart=Never \
-  --attach \
-  --rm \
-  --overrides="$(helm template \
-    --values .kube/staging.yaml \
-    --set-string processes.api.image.tag=${TAG} \
-    --show-only templates/deployment.yaml \
-    pokedextracker/app \
-    | yq -o=json - \
-    | jq -s '{
-      "spec": {
-        "containers": [{
-          "name": "migrations-'"${TAG}"'",
-          "image": "'"${REPO}"':'"${TAG}"'",
-          "command": ["migrations", "migrate"],
-          "env": .[0].spec.template.spec.containers[0].env
-        }]
-      }
-    }'
-  )" \
-  --command -- migrations migrate
-
-echo
-echo -e "\033[1;32m==> Deploying ${TAG} to staging on ${KUBE_CONTEXT}...\033[0m"
-echo
-
-helm upgrade \
-  --kube-context ${KUBE_CONTEXT} \
-  --install backend \
-  --version ${CHART_VERSION} \
-  --namespace staging \
-  --values .kube/staging.yaml \
-  --set-string processes.api.image.tag=${TAG} \
-  --wait \
-  pokedextracker/app
+fly deploy \
+  --config .fly/staging.toml \
+  --image ${REPO}:${TAG} \
+  --vm-size shared-cpu-1x
 
 echo
 echo -e "\033[1;33m==> Deployed to staging. If everything looks good and you want to deploy to"
@@ -102,51 +49,15 @@ read proceed
 
 if [ "${proceed}" == "yes" ]; then
   echo
-  echo -e "\033[1;32m==> Running migrations from ${TAG} on production...\033[0m"
+  echo -e "\033[1;32m==> Deploying ${TAG} to production...\033[0m"
   echo
 
-  kubectl run \
-    --context ${KUBE_CONTEXT} \
-    migrations-${TAG} \
-    --namespace production \
-    --image=${REPO}:${TAG} \
-    --restart=Never \
-    --attach \
-    --rm \
-    --overrides="$(helm template \
-      --values .kube/production.yaml \
-      --set-string processes.api.image.tag=${TAG} \
-      --show-only templates/deployment.yaml \
-      pokedextracker/app \
-      | yq -o=json - \
-      | jq -s '{
-        "spec": {
-          "containers": [{
-            "name": "migrations-'"${TAG}"'",
-            "image": "'"${REPO}"':'"${TAG}"'",
-            "command": ["migrations", "migrate"],
-            "env": .[0].spec.template.spec.containers[0].env
-          }]
-        }
-      }'
-    )" \
-    --command -- migrations migrate
-
-  echo
-  echo -e "\033[1;32m==> Deploying ${TAG} to production on ${KUBE_CONTEXT}...\033[0m"
-  echo
-
-  helm upgrade \
-    --kube-context ${KUBE_CONTEXT} \
-    --install backend \
-    --version ${CHART_VERSION} \
-    --namespace production \
-    --values .kube/production.yaml \
-    --set-string processes.api.image.tag=${TAG} \
-    --wait \
-    pokedextracker/app
+  fly deploy \
+    --config .fly/production.toml \
+    --image ${REPO}:${TAG} \
+    --vm-size shared-cpu-1x
 else
   echo
-  echo -e "\033[1;33m==> Skipping deploying ${TAG} to production on ${KUBE_CONTEXT}...\033[0m"
+  echo -e "\033[1;33m==> Skipping deploying ${TAG} to production...\033[0m"
   echo
 fi
